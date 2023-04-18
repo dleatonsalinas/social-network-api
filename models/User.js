ba347435f4c5b3e64b5136afbb9f7568f6b83eff
dleatonsalinas <daniellesalinas721@gmail.com>
2023-04-17 22:56:23 -0500

    update routes and models

---
+++ models/User.js
@@ -0,0 +1,56 @@
+// Models
+// User
+
+// username
+
+// String
+// Unique
+// Required
+// Trimmed
+
+// email
+// String
+// Required
+// Unique
+// Must match a valid email address (look into Mongoose's matching validation)
+
+// thoughts
+
+// Array of _id values referencing the Thought model
+// friends
+
+// Array of _id values referencing the User model (self-reference)
+// Schema Settings
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema (
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },  
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/, "Please enter a valid email address."]
+      },
+      thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+          },
+      ],
+      user: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    }
+)
